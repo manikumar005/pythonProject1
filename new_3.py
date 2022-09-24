df9e89736ce88c427f22b18809179731261d9202
admin <manikumargodavarthi125@gmail.com>
2022-09-24 17:17:32 +0530

    one line is updated

---
+++ new_3.py
@@ -0,0 +1,17 @@
+# import re
+# ip="192.121.52.63"
+# exp=("(25[0-5]|1[0-9][0-9]|1[1-9]?[0-9]\.{3})(25[0-5]|1[0-9][0-9]|1[1-9]?[0-9])")
+# m=re.match(exp,ip)
+# if m:
+#     print(m.group(),"valip ip")
+# else:print("not valid ip")
+
+# string = "Today is monday"
+# #"yadoT si yadnom"
+# # s=str[::-1]
+# s1=string.split()
+# # print(s1)
+# x=(" ".join(s1[::-1]))
+# print(x[::-1])
+
+print("now it is updated to github")
